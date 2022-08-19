47aedebf5286abff803293f367c73fdaef54ba25
zhangyc <1643935980@qq.com>
2022-08-19 14:45:39 +0800

    feat: commit

--- src/main/java/com/example/boot/annotation/RoleListenerQueue.java
+++ src/main/java/com/example/boot/annotation/RoleListenerQueue.java
@@ -8,6 +8,6 @@ import java.lang.annotation.*;
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@RabbitListener(queues = {"t_role"},ackMode = "MANUAL")
+@RabbitListener(queues = {"user"},ackMode = "MANUAL")
 public @interface RoleListenerQueue {
 }
--- src/main/java/com/example/boot/listener/RoleMQListener.java
+++ src/main/java/com/example/boot/listener/RoleMQListener.java
@@ -22,10 +22,10 @@ public class RoleMQListener {
 
     @RabbitHandler
     public void opHandler(String customTag, byte[] body) {
-        System.out.println(customTag);
         //todo 消费消息插入日志   记录消费成功  消费消息根据deliveryTag校验是否重复消费
         String s = new String(body, StandardCharsets.UTF_8);
         JSONArray objects = JSONArray.of(s);
         objects.forEach(System.out::println);
+
     }
 }
