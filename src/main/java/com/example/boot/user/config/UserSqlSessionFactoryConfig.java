65b0dd646818272256f60003f151fef6892dfdad
zhangyc <1643935980@qq.com>
2022-08-29 17:09:40 +0800

    feat: config jta many database

--- src/main/java/com/example/boot/user/config/UserSqlSessionFactoryConfig.java
+++ src/main/java/com/example/boot/user/config/UserSqlSessionFactoryConfig.java
@@ -6,8 +6,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
